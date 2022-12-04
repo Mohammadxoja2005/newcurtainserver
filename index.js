68a7558dc4b52c8cfe63c4254b6781bb24d4fcdd
Muhammadxoja <rustamxojayev.mohammadxoja@yandex.ru>
2022-12-04 19:20:31 +0500

    changes in index

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const app = express();
-const db = require("./models");
+// const db = require("./models");
 const cors = require("cors");
-const quote = require("./routes/quote");
+// const quote = require("./routes/quote");
 const PORT = 5000;
 // require("dotenv").config(); 
 
 app.use(express.json());
 app.use(cors());
 
-db.sequelize.sync().then(() => {
+// db.sequelize.sync().then(() => {
   app.listen(process.env.PORT || 5000, () => {
     console.log("server started...");
   });
-});
+// });
 
-app.use("/quote", quote);
+// app.use("/quote", quote);
 
 app.get("/", (req, res) => {
   res.send("hello world");
