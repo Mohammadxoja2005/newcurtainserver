b28fcc47d8e502c6de8340931297eabebbcc5247
Muhammadxoja <rustamxojayev.mohammadxoja@yandex.ru>
2022-12-04 21:39:41 +0500

    vjlkjdsfs

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
-const db = require("./models");
 const cors = require("cors");
-const quote = require("./routes/quote");
-const PORT = 5000;
+const quote = require("./routes/quote"); 
+const PORT = 5000; 
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
+
+const db = require("./models");
+
 
 db.sequelize.sync().then(() => {
   app.listen(process.env.PORT || 5000, () => {
