29d9a07036a6a3474d9891269baa6acef458c77e
Muhammadxoja <rustamxojayev.mohammadxoja@yandex.ru>
2022-12-04 20:55:35 +0500

    ldfkds

--- models/quote.js
+++ models/quote.js
@@ -1,15 +1,13 @@
-'use strict'; 
-
 module.exports = (sequelize, DataTypes) => {
-    const Quote = sequelize.define("Quote", {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      phone: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    });
-    return Quote;
-  };
+  const Quote = sequelize.define("Quote", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return Quote;
+};
